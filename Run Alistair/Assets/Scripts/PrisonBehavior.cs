9cc1ec37e6a46aa39a4faf71b874866565682f3d
lennieok <elena.o.kosowski@gmail.com>
2021-04-24 22:05:33 -0400

    added audio when bunny spawns

--- Run Alistair/Assets/Scripts/PrisonBehavior.cs
+++ Run Alistair/Assets/Scripts/PrisonBehavior.cs
@@ -6,17 +6,25 @@ public class PrisonBehavior : MonoBehaviour
 {
     public GameObject bunny;
     public GameObject[] DeathWall;
+    private bool left = false;
+    AudioSource AS;
+
+    void Start(){
+
+        AS = GetComponent<AudioSource>();
+    }
 
   void OnTriggerExit(Collider other){
 
-        if(other.gameObject.CompareTag("Player")){
+        if(other.gameObject.CompareTag("Player") && !left){
 
             Debug.Log("Player Has Left Prison");
+            AS.Play(0);
             bunny.SetActive(true);
-
             foreach(GameObject DW in DeathWall){
             DW.SetActive(true);
             }
+            left = true;
         }
     }
 }
